6890e4e214e4813889306e5fff9c3e75bcbe8de5
German <germanarcila@gmail.com>
2022-05-26 11:56:19 -0400

    first late commit

---
+++ app.js
@@ -0,0 +1,71 @@
+var express = require("express");
+var path = require("path");
+const fs = require("fs");
+const multer = require("multer");
+const mongoose = require("mongoose");
+var imageModel = require("./models/imageModel");
+
+var app = express();
+
+// view engine setup
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, "public")));
+
+mongoose.connect(
+	process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/node-image-test",
+	{
+		useNewUrlParser: true,
+		useUnifiedTopology: true,
+	}
+);
+
+// SET STORAGE
+var storage = multer.diskStorage({
+	destination: function (req, file, cb) {
+		cb(null, "uploads");
+	},
+	filename: function (req, file, cb) {
+		cb(null, file.fieldname + "-" + Date.now());
+	},
+});
+
+var upload = multer({ storage: storage });
+
+app.get(["/", "/all"], async (req, res) => {
+	const imageResults = await imageModel.find({});
+	const bufferedResults = await imageResults.map(image => {
+		return new Buffer.from(image.img.data).toString("base64");
+	});
+	res.render("index", { images: bufferedResults });
+});
+
+app.post("/uploadphoto", upload.single("myImage"), (req, res) => {
+	var img = fs.readFileSync(req.file.path);
+	var encode_img = img.toString("base64");
+	var final_img = {
+		contentType: req.file.mimetype,
+		name: req.file.originalname,
+		img: {
+			data: new Buffer.from(encode_img, "base64"),
+			contentType: req.file.mimetype,
+		},
+	};
+	imageModel.create(final_img, function (err, result) {
+		if (err) {
+			console.log(err);
+		} else {
+			res.redirect("/all");
+		}
+	});
+});
+
+//Code to start server
+app.listen(8080, function () {
+	console.log("Server Started at PORT 8080");
+});
+
+module.exports = app;
